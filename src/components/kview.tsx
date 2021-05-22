8150238f0b398592118d25976554869a4ff2d39e
Markim Shaw <djsalinger67@live.com>
2021-05-22 14:19:16 -0500

    Add prismic support (#2)
    Co-authored-by: Markim Shaw <ms79723@gmail.com>

--- src/components/kview.tsx
+++ src/components/kview.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import icon from "../resources/icon_ky.svg";
-import { Container } from "@material-ui/core";
+
+import Prismic from "@prismicio/client";
+import { Date, Link, RichText } from "prismic-reactjs";
+import ApiSearchResponse from "@prismicio/client/types/ApiSearchResponse";
+
+const API_ENDPOINT = "https://kyrinne.cdn.prismic.io/api/v2";
+
+const Client = Prismic.client(API_ENDPOINT);
 
 export const KView: React.FC = () => {
-  const random = 1;
+  const [doc, setDocData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await Client.query(
+        Prismic.Predicates.at("document.type", "homepage")
+      );
+
+      if (response) {
+        const result = response.results[0];
+        console.log(response);
+
+        setDocData((prev) => {
+          return result;
+        });
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className="App-header">
-      <img src={icon} alt="logo" className="Lock" />
+    <div>
+      {doc && (
+        <div className="App-header">
+          <img src={icon} alt="logo" className="Lock" />
 
-      <p>
-        Forever yours
-        <span className="Kiss"> 😘</span>
-      </p>
-      <p>Love, Markim</p>
+          <h3>{doc.data.header[0].text}</h3>
+          <p>{doc.data.body[0].text}</p>
+        </div>
+      )}
+      {doc == null && (
+        <div className="App-header">
+          <img src={icon} alt="logo" className="Lock" />
+        </div>
+      )}
     </div>
   );
 };
